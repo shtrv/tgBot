ee131fe785fe1d5c7e68c2bb7ce54483e82062e8
shtrv <71714403+shtrv@users.noreply.github.com>
2024-11-03 20:35:35 +0300

    changes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const ytdl = require("ytdl-core");
+const fs = require("fs");
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on("text", async (ctx) => {
+  const url = ctx.text;
+  ytdl
+    .downloadFromInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ")
+    .pipe(fs.createWriteStream("video.mp4"));
+  ctx.reply("забебал");
+});
+
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
